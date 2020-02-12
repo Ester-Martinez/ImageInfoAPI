5507525068c5a2b4dc735079b0478ffa6a947e11
Ester-Martinez <martinez.ester@gmail.com>
2020-02-12 18:47:04 +0100

    implemented csv link in both indexes

--- imageInfoAPI/app.js
+++ imageInfoAPI/app.js
@@ -20,7 +20,6 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,26 +33,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-// default value for title local
 app.locals.title = 'Image Info API';
-
-
 
 const index = require('./routes/index.routes');
 app.use('/', index);
